73e34e4e0098468bf1ab8650928bea86d9aea98e
Pankaj Kr Singh <pankajkrims01@users.noreply.github.com>
2025-07-25 16:21:11 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use App\Http\Controllers\ImportUserController;
 
-Route::get('/import-users', [ImportUserController::class, 'showForm'])->name('import.form');
-Route::post('/import-users', [ImportUserController::class, 'import'])->name('import.users');
+Route::get('/', [ImportUserController::class, 'showForm'])->name('import.form');
+Route::post('/', [ImportUserController::class, 'import'])->name('import.users');
